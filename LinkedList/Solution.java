f5c10e82fa9f8ed794e5fc9b024524faf810a93f
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2025-12-04 07:49:57 +0200

    Added program EntryPoint

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -2,18 +2,23 @@
 import java.util.Collection;
 
 class Solution {
-public static ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr) {
-            return list2;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode listExample = new ListNode(-1); 
+        ListNode current = listExample;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
         }
-        if (list2 == nullptr) {
-            return list1;
-        }
-        if (list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }
-        list2->next = mergeTwoLists(list1, list2->next);
-        return list2;
+        
+        current.next = (list1 != null) ? list1 : list2;
+        
+        return listExample.next;
     }
 };
